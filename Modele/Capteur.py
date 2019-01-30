039857c556c76545cce97b6f9a242347f844e2d1
Thibaud Beaufils <thibaud.beaufils@gmail.com>
2019-01-30 17:37:40 +0100

    - Gestion fichiers XML, bar de chargement lors de la création du graphe - Fenetre de paramétrage - Notion de rôle et d'énergie

--- Modele/Capteur.py
+++ Modele/Capteur.py
@@ -2,14 +2,6 @@ from Modele.Noeud import Noeud
 
 
 class Capteur(Noeud):
-    def __init__(self, _pos, _vie_batterie):
-        super().__init__(_pos)
-        self.N_vie_batterie = _vie_batterie
-
-    @property
-    def N_vie_batterie(self):
-        return self.__N_vie_batterie
-
-    @N_vie_batterie.setter
-    def N_vie_batterie(self, _vie_vatterie):
-        self.__N_vie_batterie = _vie_vatterie
+    def __init__(self, _pos, _vie_batterie, _role):
+        super().__init__(_pos, _role)
+        self.C_vie_batterie = _vie_batterie
---
+++ Modele/Parametres.py
@@ -0,0 +1,12 @@
+
+
+class Parametres:
+    def __init__(self, _max_size, _marge, _max_distance, _min_distance, _nbr_capteurs, _capacitees_batteries,
+                 _nbr_puits):
+        self.P_max_size = _max_size
+        self.P_marge = _marge
+        self.P_max_distance = _max_distance
+        self.P_min_distance = _min_distance
+        self.P_nbr_capteurs = _nbr_capteurs
+        self.P_capacitees_batteries = _capacitees_batteries
+        self.P_nbr_puits = _nbr_puits
